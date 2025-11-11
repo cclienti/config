c7f32d9582cd09217b976021416e3b8104f32074
Christophe Clienti <cclienti@users.noreply.github.com>
2025-11-11 19:33:04 +0100

    Fixed the sway config path

--- sway/sway-nvidia.sh
+++ sway/sway-nvidia.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-export GBM_BACKEND=nvidia-drm
+# export GBM_BACKEND=nvidia-drm
 # export __GLX_VENDOR_LIBRARY_NAME=nvidia
-#export WLR_NO_HARDWARE_CURSORS=1
-#export WLR_RENDERER_ALLOW_SOFTWARE=1
+# export WLR_NO_HARDWARE_CURSORS=1
+# export WLR_RENDERER_ALLOW_SOFTWARE=1
 # export WLR_RENDERER=vulkan
 
 exec /usr/bin/sway --unsupported-gpu "$@"
